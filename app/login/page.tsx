3700e36cdb5036b4190b37c9499a60be4dcb99e5
aditehh <aditya.aryal888@gmail.com>
2026-01-14 22:05:57 +0545

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import AuthButton from '@/components/ui/auth-button'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function LoginPage() {
+    return (
+        <div>
+            <AuthButton/>
+        </div>
+    )
+}
---
+++ components/ui/auth-button.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import React from 'react'
+import { Button } from './button'
+import { useRouter } from 'next/navigation'
+import { Github, Loader2 } from 'lucide-react'
+import { signIn, useSession } from '@/lib/auth-client'
+import { useEffect } from 'react'
+
+export default function AuthButton() {
+    const router = useRouter()
+
+    const { data: session, isPending } = useSession();
+
+    if (isPending) {
+        return (
+            <div>
+                <Loader2 className='w-4 h-4 animate-spin' />
+                Loading...
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        if (session) {
+            router.push("/notes")
+        }
+    }, [session, router])
+
+
+
+    const signinbtn = async () => {
+        await signIn.social({
+            provider: "github"
+        })
+    }
+
+
+
+    return (
+        <div>
+            <Button onClick={signinbtn}>
+                <Github className='w-4 h-4 mr-2' />
+                signinwithgithub
+            </Button>
+        </div>
+    )
+
+}
