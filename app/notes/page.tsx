ed8c7c7b5ff45d9a4d13dd81c5aae352a7108d9e
aditehh <aditya.aryal888@gmail.com>
2026-01-18 18:43:28 +0545

    this

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { getCurrentUser } from '@/lib/auth'
+import { getAppUser } from '@/lib/auth';
 
 export default async function NotesPage() {
 
+    const user1 = await getAppUser();
+
     const user = await getCurrentUser();
     console.log(user)
+
     return (
         <div>
             thisisthenotespagehehe
+            loggedin as {user1?.email}
         </div>
     )
 }
