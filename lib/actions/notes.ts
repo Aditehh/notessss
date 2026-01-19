0eaea027a897a87a2ed922dd60f215089c6eddd3
aditehh <aditya.aryal888@gmail.com>
2026-01-20 00:24:14 +0545

    this

--- lib/actions/notes.ts
+++ lib/actions/notes.ts
@@ -1,6 +1,6 @@
 // lib/actions/notes.ts
 "use server";
-import { deleteNote } from "@/lib/notes";
+import createNote, { deleteNote } from "@/lib/notes";
 import { editNote } from "@/lib/notes";
 
 export async function deleteNoteAction(noteId: string) {
@@ -10,3 +10,5 @@ export async function deleteNoteAction(noteId: string) {
 export async function updateNoteAction(noteid: string, title: string, content: string) {
     await editNote(noteid, title, content)
 }
+
+
